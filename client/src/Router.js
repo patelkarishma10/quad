ed6afaf4565ab0aaf5c0e22e170f1f7c60e8dd07
patelkarishma10 <50954912+patelkarishma10@users.noreply.github.com>
2019-07-11 13:08:53 +0100

    Update Router.js

--- client/src/Router.js
+++ client/src/Router.js
@@ -19,7 +19,7 @@ function RoutingExample() {
             <Link to="/Unit">How to override from the Child/Parent</Link>
           </button>
         
-<p>HelloWorld</p>
+<p>Hello</p>
 
         <Route exact path="/" />
         <Route path="/Unit" component={Unit} />
